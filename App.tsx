39f14898c5057fc7825ed02d0070f261e958b200
Finn MacCool <henryfinnco@gmail.com>
2025-12-10 18:42:13 -0800

    Updated app.json

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import RootNavigator from './navigation/RootNavigator';
 import { OnboardingProvider } from './context/OnboardingContext';
 
 export default function App() {
   return (
-    <OnboardingProvider>
-      <RootNavigator />
-      <StatusBar style="light" />
-    </OnboardingProvider>
+    <SafeAreaProvider>
+      <OnboardingProvider>
+        <RootNavigator />
+        <StatusBar style="light" />
+      </OnboardingProvider>
+    </SafeAreaProvider>
   );
 }
